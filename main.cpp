6fba0a972543aac7c9af3b50e06098ea109df042
Mykhailo Semitkin <michael.semitkin@gmail.com>
2022-05-28 21:27:29 +0300

    implemented linear brute force

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+bool guess(unsigned long long from_inclusive, unsigned long long to_exclusive, unsigned long long guessed) {
+    for (unsigned long long i = from_inclusive; i < to_exclusive; i++) {
+        if (i == guessed) {
+            return true;
+        }
+    }
+    return false;
+}
+
+
+int main() {
+    unsigned long long guessed = 20;
+
+    clock_t start = clock();
+    guess(0, ULLONG_MAX, guessed);
+    clock_t finish = clock();
+    cout << "guessed number in: " << (finish - start) << endl;
+}
