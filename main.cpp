2a4804c319977fd995098914a52fc2917181407b
Mykhailo Semitkin <michael.semitkin@gmail.com>
2022-05-29 19:06:15 +0300

    used single standart for result output

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 
 using namespace std;
+using namespace std::chrono;
+
+#define MAX_NUMBER (4294967295)
 
 bool guess(unsigned long long from_inclusive, unsigned long long to_inclusive, unsigned long long guessed) {
     for (unsigned long long i = from_inclusive; i <= to_inclusive; i++) {
@@ -13,10 +16,10 @@ bool guess(unsigned long long from_inclusive, unsigned long long to_inclusive, u
 
 
 int main() {
-    unsigned long long guessed = 20;
+    unsigned long long guessed = MAX_NUMBER / 3;
 
-    clock_t start = clock();
-    guess(0, ULLONG_MAX, guessed);
-    clock_t finish = clock();
-    cout << "guessed number in: " << (finish - start) << endl;
+    auto start = steady_clock::now();
+    guess(0, MAX_NUMBER, guessed);
+    auto finish = steady_clock::now();
+    printf("S: %7.4fs\n", (double) duration_cast<microseconds>(finish - start).count() / 1000000.0);
 }
